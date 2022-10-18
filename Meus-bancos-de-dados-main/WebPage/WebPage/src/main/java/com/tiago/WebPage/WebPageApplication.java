beb9a384f02b26aa1467f7a3ec8b3586f451ea98
Tiago Lima da Anunciação <105564355+tiagolima10@users.noreply.github.com>
2022-10-18 11:02:56 -0300

    Add files via upload

---
+++ Meus-bancos-de-dados-main/WebPage/WebPage/src/main/java/com/tiago/WebPage/WebPageApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.WebPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPageApplication.class, args);
+	}
+
+}
